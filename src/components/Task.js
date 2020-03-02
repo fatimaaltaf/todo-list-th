892b87511cdef1b2557638282e376f0c2a67f6bf
Fatima Altaf <fatima.altaf@gmail.com>
2020-03-01 20:55:09 -0500

    add css style to task card title

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,54 +6,57 @@ function Task(props) {
   const { task, index, completeTask, incompleteTask, deleteTask } = props;
 
   return (
-    <div className="task">
-      <div className="pending-complete">
-        {!task.isCompleted ? (
-          <p className="pending">
-            <img
-              alt="high-importance-icon"
-              src="https://img.icons8.com/officel/35/000000/high-importance.png"
-            ></img>
-          </p>
-        ) : (
-          <p className="completed">
-            <img
-              alt="checkmark-icon"
-              src="https://img.icons8.com/cotton/35/000000/checkmark.png"
-            ></img>
-          </p>
-        )}
-      </div>
-      <div
-        className="task-title-desc"
-        style={{ textDecoration: task.isCompleted ? "line-through" : "" }}
-      >
-        <p>Task:&nbsp;{task.title}</p>
-        <p>Description:&nbsp;{task.description}</p>
-      </div>
-      <p
-        className="task-category"
-        style={{ display: task.category === "none" ? "none" : "block" }}
-      >
-        {task.category}
-      </p>
-      <div className="dueBy">
-        Due By:&nbsp;
-        {moment(task.startDate).format("MMM Do YYYY")}
-      </div>
-      <div className="complete-delete">
-        {!task.isCompleted ? (
-          <button className="complete" onClick={() => completeTask(index)}>
-            Complete
+    <div>
+      <h3 className="tasks-card-title">Tasks</h3>
+      <div className="task">
+        <div className="pending-complete">
+          {!task.isCompleted ? (
+            <p className="pending">
+              <img
+                alt="high-importance-icon"
+                src="https://img.icons8.com/officel/35/000000/high-importance.png"
+              ></img>
+            </p>
+          ) : (
+            <p className="completed">
+              <img
+                alt="checkmark-icon"
+                src="https://img.icons8.com/cotton/35/000000/checkmark.png"
+              ></img>
+            </p>
+          )}
+        </div>
+        <div
+          className="task-title-desc"
+          style={{ textDecoration: task.isCompleted ? "line-through" : "" }}
+        >
+          <p>Task:&nbsp;{task.title}</p>
+          <p>Description:&nbsp;{task.description}</p>
+        </div>
+        <p
+          className="task-category"
+          style={{ display: task.category === "none" ? "none" : "block" }}
+        >
+          {task.category}
+        </p>
+        <div className="dueBy">
+          Due By:&nbsp;
+          {moment(task.startDate).format("MMM Do YYYY")}
+        </div>
+        <div className="complete-delete">
+          {!task.isCompleted ? (
+            <button className="complete" onClick={() => completeTask(index)}>
+              Complete
+            </button>
+          ) : (
+            <button className="complete" onClick={() => incompleteTask(index)}>
+              Incomplete
+            </button>
+          )}
+          <button className="delete" onClick={() => deleteTask(index)}>
+            Delete
           </button>
-        ) : (
-          <button className="complete" onClick={() => incompleteTask(index)}>
-            Incomplete
-          </button>
-        )}
-        <button className="delete" onClick={() => deleteTask(index)}>
-          Delete
-        </button>
+        </div>
       </div>
     </div>
   );

38c949ea635215984e0a373151f64a91b95cf2be
Fatima Altaf <fatima.altaf@gmail.com>
2020-06-09 13:29:06 -0400

    Updates

---
+++ precache-manifest.87a3d5a22c3ca4d4173f75d6e247d618.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe1974974663c84db2168be00832433",
+    "url": "/todo-list-th/index.html"
+  },
+  {
+    "revision": "c54fc2e561df1d3d1115",
+    "url": "/todo-list-th/static/css/2.97aab26a.chunk.css"
+  },
+  {
+    "revision": "1e6464a40f029480085a",
+    "url": "/todo-list-th/static/css/main.207e4052.chunk.css"
+  },
+  {
+    "revision": "c54fc2e561df1d3d1115",
+    "url": "/todo-list-th/static/js/2.a831bafb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-list-th/static/js/2.a831bafb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6464a40f029480085a",
+    "url": "/todo-list-th/static/js/main.a2a674d7.chunk.js"
+  },
+  {
+    "revision": "0268647289c3e3c673d8",
+    "url": "/todo-list-th/static/js/runtime-main.51725f51.js"
+  }
+]);
